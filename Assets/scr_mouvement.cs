dcb4e95ea4914c93bc9048500097c1e6f1852e03
nigaige <nicolas.gaige@free.fr>
2019-01-19 17:50:39 +0100

    first commit

---
+++ Assets/scr_mouvement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+public class scr_mouvement : MonoBehaviour {
+	const double VIT_LATERAL = 0.5;
+	double[] RAIL_X = new double[] {45 , 50 , 55};
+	int onrail = 2;
+	int aimed_rail = 2;
+	Vector3 position;
+
+
+    Animator animator;
+    
+
+    void Start(){
+    	 animator = GetComponent<Animator>();
+
+		position = transform.position;
+    }
+    void Update(){
+		position = transform.position;
+		double aimed_X = RAIL_X[aimed_rail - 1];
+		double distance = 0;
+
+		//Debug.Log("on rail : " + onrail.ToString() + "//aimed rail : " + aimed_rail.ToString());
+    	//Debug.Log("position X : " + position.x.ToString() + "//aimed position x : " + aimed_X.ToString());
+    	
+
+    	if (aimed_rail != onrail){
+    		distance = position.x - aimed_X;
+
+    		Debug.Log("distance to rail :" + distance.ToString());
+
+    		if (Math.Abs(distance) < VIT_LATERAL){ 	//arrivé
+    			position.x = (float)aimed_X;
+    			onrail = aimed_rail;
+
+    		}else if (position.x > aimed_X){	//déplace a gauche
+    			position.x -= (float)VIT_LATERAL;
+    		}else if (position.x < aimed_X){	//déplace a droite
+    			position.x += (float)VIT_LATERAL;
+    		}
+
+
+ 			transform.position = position;
+
+    	}else {
+    		if (Input.GetKeyDown(KeyCode.LeftArrow) && onrail != 1){
+    			aimed_rail--;
+    			if (!animator.GetCurrentAnimatorStateInfo(0).IsName("jump_left")){
+    				animator.SetTrigger("move_left");
+    			}
+	            
+    		}
+    		if (Input.GetKeyDown(KeyCode.RightArrow) && onrail != 3){
+    			aimed_rail++;
+    			if (!animator.GetCurrentAnimatorStateInfo(0).IsName("jump_right")){
+	            	animator.SetTrigger("move_right");
+	        	}
+    		}
+
+
+
+    	}
+        
+    }
+}
---
+++ Assets/scr_rail.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_rail : MonoBehaviour{
+    float cart_speed = 0.5f;
+    const float view_mini = -6;
+    const float view_maxi = 90;
+    Vector3 position;
+
+
+    void Start(){
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update(){
+    	position = transform.position;
+
+    	position.z -= cart_speed;
+        if (position.z < view_mini){
+        	position.z = view_maxi;
+        }
+        transform.position = position;
+    }
+}
